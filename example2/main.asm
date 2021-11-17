23f73215eafa6fa19a754bd21b249f6daa16778d
SnailMath <snailmathprog@gmail.com>
2021-11-17 12:40:19 +0100

    Proper divide function

--- example2/main.asm
+++ example2/main.asm
@@ -7,15 +7,23 @@ mov main,jump #Jump to the main.
 .equ exit 0x0003
 .equ sp   0x00FF
 #Registers that you don't need to preserve during subroutine calls
-.equ r1   0x00fe
-.equ r2   0x00fd
+.equ r0   0x00f5
+.equ r1   0x00fe #1st arg and result of functions
+.equ r2   0x00fd #2nd arg of functions
 .equ r3   0x00fc
 .equ r4   0x00fb
-#Registers that you DO need to preserve during subroutine calls (just push them and pop them)
 .equ r5   0x00fa
 .equ r6   0x00f9
 .equ r7   0x00f8
+#Registers that you DO need to preserve during subroutine calls (just push them and pop them)
 .equ r8   0x00f7
+.equ r9   0x00f6
+.equ r10  0x00f5
+.equ r11  0x00f4
+.equ r12  0x00f6
+.equ r13  0x00f5
+.equ r14  0x00f4
+.equ r15  0x00f6
 
 main:
 	.word $+1
@@ -78,7 +86,16 @@ main:
 	mov $+6,
 	mov divide,jump
 	.word $+1
+	#result is in r1, rest is in r2
+	mov r2,r8 #back up rest
 	#result is still in r1
+	add n1, sp
+	mov sp, $+5
+	mov $+6,
+	mov print_int_ln,jump
+	.word $+1
+	#put rest in r1
+	mov r8,r1
 	add n1, sp
 	mov sp, $+5
 	mov $+6,
@@ -121,8 +138,10 @@ main:
 
 num1:
 	.word 1234
+	#.word 400
 num2:
 	.word 23
+	#.word 2
 
 
 text1:
--- example2/math.asm
+++ example2/math.asm
@@ -45,27 +45,80 @@ mulyes:
 	.word mulback
 
 
-divide:
-	.word $+1
+divide: #The new divide
 	#divide
-	mov r2,r3
-	xor n1,r3
-	add p1,r3 #r3 is now the 2's complemnt of r2
-	mov n1,r4
-divloop:
-	add p1,r4
-	add r3,r1
-	mif a_divloop,jump
-	
-	mov r4,r1
+	.word $+1
+	#r1 divident r2 divisor r3 divisorbackup r4 bit counter/leading 0's r5 result r6 negative divisor
+	#back up the divisor
+	mov r2,r3 
+ 	#check if the divisor is 0
+	add n1,r2
+	mif nodiverr,jump
+		mov diverr,jump
+	nodiverr:
+	.word $+1
+
+	mov r3, r2  # restore the divisor (destroyed by the ==0 test)
+	mov null,r5 # init result to 0
+	mov n1, r4  # init the leading-0's-counter to -1
+	#count leading 0's
+divl1:
+		add p1,r4
+		sft p1,r2 #shift left and count up
+		mif divl1break,jump # break if we reached the first non-0 bit
+	mov $+3,jump #loop
+	.word divl1
+divl1break:
+	.word $+1
+
+	#now we've got the number of leading 0's in r4
+
+	#now check how often the number fits into the divient
+divl2:
+		sft p1,r5 #shift the result to the left to make space for the new digit on the right
+		mov r3,r2 #restore the divisor	
+		sft r4,r2 #first shift the divisor left (aka ignore the leading 0's in the first run of the loop)
+		#subtract divisor from the divident. (This works eiter 0 or 1 times)
+		mov r2,r6 #create the negarive of the divisor
+		xor n1,r6
+		add p1,r6 #r6 is now the negative of the divisor
+		add r6,r1 #subtract
+		mif asubworked,jump #test if the subtraction worked
+		#sub didn't work
+			#if the subtraction was less than 0, we have to undo it.
+			add r2,r1
+			mov adivifend,jump
+		#sub did work
+		subworked:
+			#if the subtraction worked, the number fits in there 1 times, so this bit is a 1
+			add p1,r5
+		divifend:
+		#decrement the shift counter, so we compare one byte further right
+		add n1,r4
+		#if r4 wasn't 0, repeat.
+	mif adivl2,jump
+
+	#put the rest in r2 and the result in r1
+	mov r1,r2
+	mov r5,r1
 
 	#return
 	mov sp, $+7
 	add p1, sp
 	mov   , 0
 
-a_divloop:
-	.word divloop
+adivl2:
+	.word divl2
+asubworked:
+	.word subworked
+adivifend:
+	.word divifend
+diverr: #Print division error message!
+	.word $+1
+	mov $+6,r1
+	mov print,jump
+	.word $+1
+	.string "Division by 0!\n"
 
 a_mulloop:
 	.word mulloop
